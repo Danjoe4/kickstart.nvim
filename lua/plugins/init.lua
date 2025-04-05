9b1690c753dde501d7c69ce917f198b0baae2123
Daniel Broderick <djbroderick4@gmail.com>
2025-04-05 12:18:15 -0400

    switch to nvchad

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -12,6 +12,16 @@ return {
       require "configs.lspconfig"
     end,
   },
+  {
+    "joshuavial/aider.nvim",
+    opts = {
+      -- your configuration comes here
+      -- if you don't want to use the default settings
+      auto_manage_context = true, -- automatically manage buffer context
+      default_bindings = true, -- use default <leader>A keybindings
+      debug = false, -- enable debug logging
+    },
+  },
 
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
